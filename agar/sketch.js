145e95bc616301a791cddb73aa0d156ad41e4fa9
Datnt <datnt@nal.vn>
2018-07-18 10:54:00 +0000

    step 02.1

--- agar/sketch.js
+++ agar/sketch.js
@@ -1,13 +1,22 @@
 var board;
-var wcell=20;
+var wcell = 20;
+
 function setup() {
     createCanvas(640, 480)
-    rows=floor(480/wcell)
-    cols=floor(640/wcell)
-    board=create2DArray(rows,cols)
+    rows = floor(480 / wcell)
+    cols = floor(640 / wcell)
+    board = create2DArray(rows, cols)
     smooth()
 }
 
 function draw() {
-    display(board,wcell)  
+    display(board, wcell)
+}
+
+function mousePressed() {
+    var pos = new Array();
+    var x = mouseX % 20 == 0 ? Math.ceil(mouseX / 20 - 1) : Math.ceil(mouseX / 20);
+    var y = mouseY % 20 == 0 ? Math.ceil(mouseY / 20 - 1) : Math.ceil(mouseY / 20);
+    console.log(x + "-" + y);
+    seeding(y - 1, x - 1, board);
 }
