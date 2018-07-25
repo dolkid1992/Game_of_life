541e8908dd5beb509fe08c430d82a0285c4f40e0
Datnt <dol.mouse92@gmail.com>
2018-07-25 11:33:04 +0700

    update checkPattern glider life

--- agar/core.js
+++ agar/core.js
@@ -5,13 +5,13 @@ const GILDER = 3;
 function display(board, rows, columns, size) {
     for (i = 0; i < rows; i++) {
         for (j = 0; j < columns; j++) {
-            coloring(color(0, 255, 0), isLivingCell(board[i][j]));
+            coloring(board[i][j]);
             rect(j * size, i * size, size, size)
         }
     }
 }
 
-function coloring(color, condition) {
+function coloring(condition) {
     if (condition === LIVING) {
         fill(0, 255, 0)
     }
@@ -32,7 +32,7 @@ function seeding(row, col, board) {
 }
 
 function isLivingCell(value) {
-    return value;
+    return value == 1 || value == 2 || value == 3;
 }
 
 function create2DArray(rows, cols) {
@@ -48,7 +48,7 @@ function create2DArray(rows, cols) {
 
 // The process of creating the new generation
 function generate(board, rows, columns, next) {
-    resetColor(board, rows, columns)
+    //resetColor(board, rows, columns)
     // Loop through every spot in our 2D array and check spots neighbors
     for (var x = 1; x < rows - 1; x++) {
         for (var y = 1; y < columns - 1; y++) {
@@ -56,22 +56,27 @@ function generate(board, rows, columns, next) {
             var neighbors = 0;
             for (var i = -1; i <= 1; i++) {
                 for (var j = -1; j <= 1; j++) {
-                    neighbors += board[x + i][y + j];
+                    neighbors += isLivingCell(board[x + i][y + j]) ? 1 : 0;
                 }
             }
 
-            neighbors -= board[x][y];
+            neighbors -= isLivingCell(board[x][y]) ? 1 : 0;
 
-            if ((board[x][y] === 1) && (neighbors < 2)) next[x][y] = 0;
-            else if ((board[x][y] === 1) && (neighbors > 3)) next[x][y] = 0;
+            if ((isLivingCell(board[x][y])) && (neighbors < 2)) next[x][y] = 0;
+            else if ((isLivingCell(board[x][y])) && (neighbors > 3)) next[x][y] = 0;
             else if ((board[x][y] === 0) && (neighbors === 3)) next[x][y] = 1;
             else {
-                if (next[x][y] !== STILL_LIFES)
-                    next[x][y] = board[x][y];
+                //if (next[x][y] !== STILL_LIFES)
+                    next[x][y] = isLivingCell(board[x][y]) ? 1 : 0;
             }
 
-            checkStillLifeBlock(board, x, y, next);
-            checkPattern(board, x, y);
+            // checkStillLifeBlock(board, x, y, next);
+        }
+    }
+
+    for (var x = 1; x < rows - 1; x++) {
+        for (var y = 1; y < columns - 1; y++) {
+            checkPattern(next, x, y);
         }
     }
 
@@ -93,45 +98,33 @@ function reset(arr, rows, columns) {
     }
 }
 
-function resetColor(arr, rows, columns) {
-    for (var x = 0; x < rows - 1; x++) {
-        for (var y = 0; y < columns - 1; y++) {
-            if (arr[x][y] > 1)
-                arr[x][y] = 1;
-        }
-    }
-}
-
-function checkStillLifeBlock(board, x, y, next) {
-    var checkBlock = false;
-    var checkBoderBlock = false;
-    if (board[x][y] === 1) {
-        checkBlock = (board[x + 1][y + 0] === 1) && (board[x + 0][y + 1] === 1) && (board[x + 1][y + 1] === 1) && (board[x + 0][y + 0] === 1);
-        checkBoderBlock = (board[x - 1][y - 1] === 0) && (board[x + 0][y - 1] === 0) && (board[x + 1][y - 1] === 0)
-            && (board[x + 2][y - 1] === 0) && (board[x - 1][y + 0] === 0) && (board[x + 2][y + 0] === 0)
-            && (board[x - 1][y + 1] === 0) && (board[x + 2][y + 1] === 0) && (board[x - 1][y + 2] === 0)
-            && (board[x + 0][y + 2] === 0) && (board[x + 1][y + 2] === 0) && (board[x + 2][y + 2] === 0)
-    }
-
-    if (checkBlock && checkBoderBlock) {
-        next[x + 0][y + 0] = STILL_LIFES;
-        next[x + 0][y + 1] = STILL_LIFES;
-        next[x + 1][y + 0] = STILL_LIFES;
-        next[x + 1][y + 1] = STILL_LIFES;
-    }
-}
-
 function checkPattern(board, x, y) {
     if (matrixCompare(board, x, y, pattern1, 0, 0, 5, 5)) {
         changePatternColor(board, x, y, 5, 5, GILDER);
+        return;
+    }
+    if (matrixCompare(board, x, y, pattern2, 0, 0, 5, 5)) {
+        changePatternColor(board, x, y, 5, 5, GILDER);
+        return;
+    }
+    if (matrixCompare(board, x, y, pattern3, 0, 0, 5, 5)) {
+        changePatternColor(board, x, y, 5, 5, GILDER);
+        return;
+    }
+    if (matrixCompare(board, x, y, pattern4, 0, 0, 5, 5)) {
+        changePatternColor(board, x, y, 5, 5, GILDER);
+        return;
+    }
+    if (matrixCompare(board, x, y, pattern_block, 0, 0, 4, 4)) {
+        changePatternColor(board, x, y, 5, 5, STILL_LIFES);
     }
 }
 
 //check if matrix a = b, size m x n
 function matrixCompare(a, i, j, b, k, l, m, n) {
-    for (let o = i; o < i + m; o++) {
-        for (let p = j; p < j + n; p++) {
-            if (a[o][p] !== b[o][p]) {
+    for (let o = 0; o < m; o++) {
+        for (let p = 0; p < n; p++) {
+            if (a[o + i][p + j] !== b[o][p]) {
                 return false;
             }
         }
@@ -140,27 +133,28 @@ function matrixCompare(a, i, j, b, k, l, m, n) {
 }
 
 function changePatternColor(board, x, y, m, n, color) {
-    for (let i = x; i < x + m; i++) {
-        for (let j = y; j < y + m; j++) {
-            if (board[i][j] === 1) {
-                board[i][j] = color;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < m; j++) {
+            if (board[i + x][j + y] === 1) {
+                board[i + x][j + y] = color;
             }
         }
     }
+
 }
 
 
 var pattern1 = new Array();
 pattern1[0] = [0, 0, 0, 0, 0];
-pattern1[1] = [0, 0, 1, 0, 0];
-pattern1[2] = [1, 0, 1, 0, 0];
-pattern1[3] = [0, 1, 1, 0, 0];
+pattern1[1] = [0, 0, 0, 1, 0];
+pattern1[2] = [0, 1, 0, 1, 0];
+pattern1[3] = [0, 0, 1, 1, 0];
 pattern1[4] = [0, 0, 0, 0, 0];
 
 var pattern2 = new Array();
 pattern2[0] = [0, 0, 0, 0, 0];
 pattern2[1] = [0, 1, 0, 0, 0];
-pattern2[2] = [1, 0, 1, 1, 0];
+pattern2[2] = [0, 0, 1, 1, 0];
 pattern2[3] = [0, 1, 1, 0, 0];
 pattern2[4] = [0, 0, 0, 0, 0];
 
@@ -173,9 +167,15 @@ pattern3[4] = [0, 0, 0, 0, 0];
 
 var pattern4 = new Array();
 pattern4[0] = [0, 0, 0, 0, 0];
-pattern4[1] = [0, 0, 0, 0, 0];
-pattern4[2] = [0, 1, 0, 1, 0];
-pattern4[3] = [0, 0, 1, 1, 0];
-pattern4[4] = [0, 0, 1, 0, 0];
+pattern4[1] = [0, 1, 0, 1, 0];
+pattern4[2] = [0, 0, 1, 1, 0];
+pattern4[3] = [0, 0, 1, 0, 0];
+pattern4[4] = [0, 0, 0, 0, 0];
+
+var pattern_block = new Array();
+pattern_block[0] = [0, 0, 0, 0];
+pattern_block[1] = [0, 1, 1, 0];
+pattern_block[2] = [0, 1, 1, 0];
+pattern_block[3] = [0, 0, 0, 0];
 
 
